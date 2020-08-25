ab4ac924e530ba795852d0114d530acc78df5cf1
Peter Dickten <peter.dickten@dcs-fuerth.de>
2020-08-25 14:45:50 +0200

    03-07

--- ilovecoffee/src/coffees/coffees.controller.ts
+++ ilovecoffee/src/coffees/coffees.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
 
-    @Get('all')
-    findAll() {
-        return '... wird alle Kaffeesorten liefern.'
+    @Get('')
+    findAll(): any {
+        return '... wird alle Kaffeesorten liefern.';
     }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): any {
+        return `... wird die Kaffeesorte für die ID ${id} liefern.`;
+    }
+
 }
