d9661af2d31227a6a77b1ed31602e08e776973d0
Peter Dickten <peter.dickten@dcs-fuerth.de>
2020-09-06 21:21:23 +0200

    03-36

--- ilovecoffee/src/app.service.ts
+++ ilovecoffee/src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor() {
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+  }
   getHello(): string {
     return 'Hello workshop!';
   }
--- ilovecoffee/src/coffees/coffees.module.ts
+++ ilovecoffee/src/coffees/coffees.module.ts
@@ -8,12 +8,26 @@ import { Event } from '../events/entities/event.entity';
 import { MockCoffeesService } from './mock-coffees.service';
 import { COFFEE_BRANDS } from './coffee.constants';
 
+
+class ConfigService { }
+class DevelopmentConfigService { }
+class ProductionConfigService { }
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         { provide: CoffeesService, useValue: new MockCoffeesService() },
         { provide: COFFEE_BRANDS, useValue: ['Starbucks', 'TCHIBO'] },
+        {
+            provide: ConfigService, useClass:
+                process.env.NODE_END === 'development' ?
+                    DevelopmentConfigService :
+                    ProductionConfigService
+        },
+
+
+
     ],
     exports: [CoffeesService]
 })
