5b12dd89d241cc411ad43c45e4fc13421e1bc198
Peter Dickten <peter.dickten@dcs-fuerth.de>
2020-09-09 00:40:45 +0200

    03-46

--- ilovecoffee/src/app.module.ts
+++ ilovecoffee/src/app.module.ts
@@ -10,18 +10,22 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true, // in Produktion: false,
+        logging: false
+      })
+    }),
     ConfigModule.forRoot({ load: [appConfig] }),
-    CoffeesModule, TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // in Produktion: false,
-      logging: false
-    }), CoffeeRatingModule, DatabaseModule],
+    CoffeesModule,
+    CoffeeRatingModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
