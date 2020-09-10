0b7179b1f9f8cb3e8f4acb035695e02f7785e61c
Peter Dickten <peter.dickten@dcs-fuerth.de>
2020-09-10 18:36:51 +0200

    03-54

--- ilovecoffee/src/main.ts
+++ ilovecoffee/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,15 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }));
+
+  const options = new DocumentBuilder()
+    .setTitle('I love coffee')
+    .setDescription('coffee, lots of coffee')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
